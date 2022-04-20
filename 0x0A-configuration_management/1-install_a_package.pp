38c7af86e77daeb1778ed91e0384b36a3238489c
error-rgl <rglcnd@gmail.com>
2022-04-20 17:58:21 +0200

    [Update]: script that install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,14 @@
-# Installs puppet-lint, in /tmp version 2.5.0
+# Installs Flask, in /tmp version 2.1.0
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'http://rubygems.org',
+#package { 'puppet-lint':
+#  ensure   => '2.5.0',
+#  provider => 'gem',
+#  source   => 'http://rubygems.org',
+#}
+
+
+exec {'flask_install':
+  command  => 'pip3 install Flask==2.1.0',
+  provider => shell,
+  #path    => ['/usr/bin', '/bin'],
 }
