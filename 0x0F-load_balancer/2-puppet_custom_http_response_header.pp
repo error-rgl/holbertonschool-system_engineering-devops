90807d81af2d61fd49775cdc2ccbd850b2e99516
error-rgl <rglcnd@gmail.com>
2022-04-27 06:42:34 +0200

    [Feature]: creating a custom HTTP header response whit ppupet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,14 @@
+# creating a custom HTTP header response whit ppupet
+exec {'http_header':
+  provider => shell,
+  command  => 'sudo apt update -y ;\
+               sudo apt-get install nginx -y ;\
+               sudo chown -R ubuntu /var/www/ ;\
+               sudo echo "Hello World" > /var/www/html/index.nginx-debian.html ;\
+               sudo echo "Ceci n'est pas une page" > /var/www/html/error-404.html ;\
+               sudo service nginx start ;\
+               sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=NdYWuo9OFAw permanent;/" /etc/nginx/sites-available/default ;\
+               sudo sed -i "s/^server\s{/server {\n\terror_page 404 \/error-404.html;/1" /etc/nginx/sites-available/default ;\
+               sudo sed -i "s/^server\s{/server {\n\tadd_header X-Served-By $HOSTNAME;/1" /etc/nginx/sites-available/default ;\
+               sudo service nginx restart',
+}
